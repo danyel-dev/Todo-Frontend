f1bec599fd3692ed847f3990317ad9ee457598dc
danyel-dev <dannykill4221x@gmail.com>
2023-05-19 22:32:46 -0300

    getting api token and saving to local storage

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import axios from "axios";
+import UserLists from "./UserLists";
 
 export default function Login() {
+    const [token, setToken] = useState("");
+
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
@@ -12,15 +16,37 @@ export default function Login() {
         setPassword(event.target.value)
     }
     
+
     function handleOnSubmit(event) {
-        alert('oi')
+        const URL = "http://127.0.0.1:8000/api-token-auth/"
+        
+        axios.post(URL, {username: username, password: password})
+        .then(response => {
+            localStorage.setItem('token', response.data.token)
+            setToken(response.data.token)
+        })
+        
+        event.preventDefault()
     }
 
+    function handleLogout() {
+        localStorage.removeItem('token')
+        setToken("")
+    }
+
+    if(!token) 
+        return (
+            <form onSubmit={handleOnSubmit}>
+                <input type="text" value={username} onChange={handleChangeUsername} placeholder="username" />
+                <input type="password" value={password} onChange={handleChangePassword} placeholder="password" />
+                <button>entrar</button>
+            </form>
+        );
+
     return (
-        <form onSubmit={handleOnSubmit}>
-            <input type="text" value={username} onChange={handleChangeUsername} placeholder="username" />
-            <input type="password" value={password} onChange={handleChangePassword} placeholder="password" />
-            <button>entrar</button>
-        </form>
+        <>
+            <UserLists /> 
+            <button onClick={handleLogout}>Logout</button>  
+        </>
     );
 };
